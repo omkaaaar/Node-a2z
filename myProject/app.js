0d0d84027c45f0ec41be56f37404678b5b9c0b70
omkaaaar <75777792+omkaaaar@users.noreply.github.com>
2025-07-14 23:01:21 +0530

    Add @types/node dependency and update app.js for improved MongoDB operations

--- myProject/app.js
+++ myProject/app.js
@@ -1,37 +1,66 @@
-const { MongoClient} = require('mongodb')
-const dotEnv = require('dotenv')
-dotEnv.config()
+const { MongoClient } = require("mongodb");
+const dotEnv = require("dotenv");
+dotEnv.config();
 
-const url = process.env.MONGO_URI
-const client = new MongoClient(url)
+const url = process.env.MONGO_URI;
+const client = new MongoClient(url);
 
 //! Specify the database name
-const dbName = 'users'
+const dbName = "users";
 
-async function main(){
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Connected to database')
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected to database");
 
-    const db = client.db(dbName)
-    const collection = db.collection('HelloWorld')
+  const db = client.db(dbName);
+  const collection = db.collection("HelloWorld");
 
-    // ! Inserting data
-    const insertResult = await collection.insertMany(
-        [{"Name":"Omi", "agr":34,"city":"goaaaaaaaaa"},
-            {"Name":"Omyao","agr":24,"city":"goaaaaaaaaa"},
-            {"Name":"Omyaoooooo","agr":24,"city":"goaaaaaaaaa"}
+  // ! Inserting data
+  // const insertResult = await collection.insertMany(
+  //     [
+  //         {"Name":"Omi", "agr":34,"city":"goaaaaaaaaa"},
+  //         {"Name":"Omyao","agr":24,"city":"goaaaaaaaaa"},
+  //         {"Name":"Omyaoooooo","agr":24,"city":"goaaaaaaaaa"}
 
-        ]
-    )
-    console.log('Inserted documents =>', insertResult);
-    
-    //! Reading file
-    const findResult = await collection.find({}).toArray()
-    console.log('Found document =>', findResult);
-    
+  //     ]
+  // )
+  // console.log('Inserted documents =>', insertResult);
 
-    return 'done'
+  // const data = {
+  //         "name": "narayan",
+  //         "age": 25,
+  //         "city": "belgaum"
+  //     }
+
+  // collection.insertOne(data)
+
+  // ! UPDATE query
+  //   await collection.updateOne(
+  //     { Name: "omi the homiew" },
+  //     {
+  //       $set: {
+  //         agr: 30,
+  //       },
+  //     }
+  //   );
+
+  // ! Deleting data
+  //   const deleteEntry = await collection.deleteMany({
+  //     Name: "Omyaoooooo",
+  //   });
+  //   console.log("deleteEntry=>", deleteEntry);
+
+  //! Reading file
+  const findResult = await collection.find({}).toArray();
+
+  console.log("📦 Found document =>", findResult);
+
+  // !Counting the number of documents with similar data
+  // const result = await collection.countDocuments({Name:"Omyaoooooo"})
+  // console.log(result);
+
+  return "done";
 }
 
 main()
